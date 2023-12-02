8e76324607c872f3eab0c51e7b6d1afe67d79ad2
keelus <hugomoreda@hotmail.com>
2023-12-02 15:34:35 +0100

    :art: Add support for part 1 (removed code). Change to camelCase

--- day-1/main.go
+++ day-1/main.go
@@ -11,14 +11,14 @@ import (
 var SPELLED_NUMBERS = [...]string{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
 var DIGIT_NUMBERS = [...]rune{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
 
-func get_spelled(index int, line string) (bool, rune) {
+func getSpelled(index int, line string) (bool, rune) {
 	if index >= len(line) {
 		return false, ' '
 	}
 
-	for n, spelled_word := range SPELLED_NUMBERS {
-		if index+len(spelled_word) <= len(line) {
-			if spelled_word == line[index:index+len(spelled_word)] {
+	for n, spelledWord := range SPELLED_NUMBERS {
+		if index+len(spelledWord) <= len(line) {
+			if spelledWord == line[index:index+len(spelledWord)] {
 				return true, DIGIT_NUMBERS[n]
 			}
 		}
@@ -26,7 +26,7 @@ func get_spelled(index int, line string) (bool, rune) {
 	return false, ' '
 }
 
-func is_number(char rune) bool {
+func isNumber(char rune) bool {
 	for _, number := range DIGIT_NUMBERS {
 		if number == char {
 			return true
@@ -35,40 +35,40 @@ func is_number(char rune) bool {
 	return false
 }
 
-func get_calibration_value(line string) int {
-	first_number := ' '
-	last_number := ' '
+func getCalibrationValue(line string, parseSpelled bool) int {
+	firstNumber := ' '
+	lastNumber := ' '
 
 	for index, char := range line {
-		if is_number(char) {
-			if first_number == ' ' {
-				first_number = char
+		if isNumber(char) {
+			if firstNumber == ' ' {
+				firstNumber = char
 			}
-			last_number = char
-		} else {
-			is_spelled, got_char := get_spelled(index, line)
-			if is_spelled {
-				if first_number == ' ' {
-					first_number = got_char
+			lastNumber = char
+		} else if parseSpelled {
+			isSpelled, gotChar := getSpelled(index, line)
+			if isSpelled {
+				if firstNumber == ' ' {
+					firstNumber = gotChar
 				}
-				last_number = got_char
+				lastNumber = gotChar
 			}
 		}
 	}
 
-	final_number_str := fmt.Sprintf("%c%c", first_number, last_number)
-	final_number, err := strconv.ParseInt(final_number_str, 10, 32)
+	sFinalNumber := fmt.Sprintf("%c%c", firstNumber, lastNumber)
+	finalNumber, err := strconv.ParseInt(sFinalNumber, 10, 32)
 	if err != nil {
-		log.Fatalf("Error while parsing String to Integer for '%s'\n", final_number_str)
+		log.Fatalf("Error while parsing String to Integer for '%s'\n", sFinalNumber)
 	}
 
-	return int(final_number)
+	return int(finalNumber)
 }
 
-func get_calibration_sum(lines []string) int {
+func getCalibrationSum(lines []string, parseSpelled bool) int {
 	sum := 0
 	for _, line := range lines {
-		val := get_calibration_value(line)
+		val := getCalibrationValue(line, parseSpelled)
 		sum += val
 	}
 
@@ -89,5 +89,6 @@ func main() {
 		lines = append(lines, scanner.Text())
 	}
 
-	fmt.Printf("Total calibration sum: %d\n", get_calibration_sum(lines))
+	fmt.Printf("[Part 1] Total calibration sum: %d\n", getCalibrationSum(lines, false))
+	fmt.Printf("[Part 2] Total calibration sum: %d\n", getCalibrationSum(lines, true))
 }
