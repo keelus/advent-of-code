666c005ca5f47fcf9efefd6efb4a38121835308f
keelus <hugomoreda@hotmail.com>
2023-12-06 21:01:15 +0100

    :zap: Add parallelism to day 5's part two.

--- day05/almanac/almanac.go
+++ day05/almanac/almanac.go
@@ -3,6 +3,8 @@ package almanac
 import (
 	"advent-of-code-23/day05/convertionMapList"
 	"math"
+	"sync"
+	"sync/atomic"
 
 	"golang.org/x/exp/slices"
 )
@@ -26,22 +28,35 @@ func (a *Almanac) GetLowestNearestLocationPart1() int {
 }
 
 func (a *Almanac) GetLowestNearestLocationPart2() int {
-	smallest := math.MaxInt
+	var smallest int64 = math.MaxInt
+
+	var wg sync.WaitGroup
 
 	for i := 0; i < len(a.Seeds); i += 2 {
+		wg.Add(1)
 		beginning := a.Seeds[i]
 		length := a.Seeds[i+1]
 
-		for j := beginning; j < beginning+length; j++ {
-			value := j
-			for k, convertionMapList := range a.ConvertionMaps {
-				value = convertionMapList.GetNumberMapped(value)
-				if k == 6 && value < smallest {
-					smallest = value
+		go func(beginning int, length int) {
+			defer wg.Done()
+			curSmallest := math.MaxInt
+
+			for j := beginning; j < beginning+length; j++ {
+				value := j
+				for k, convertionMapList := range a.ConvertionMaps {
+					value = convertionMapList.GetNumberMapped(value)
+					if k == 6 && value < curSmallest {
+						curSmallest = value
+					}
 				}
 			}
-		}
+
+			atomic.StoreInt64(&smallest, int64(math.Min(float64(atomic.LoadInt64(&smallest)), float64(curSmallest))))
+
+		}(beginning, length)
 	}
 
-	return smallest
+	wg.Wait()
+
+	return int(smallest)
 }
