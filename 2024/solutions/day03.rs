d3073fea404b25c87042dffd72795a40c88577cc
keelus <hugomoreda@hotmail.com>
2024-12-03 11:00:14 +0100

    Optimized day 3.

--- 2024/solutions/day03.rs
+++ 2024/solutions/day03.rs
@@ -1,46 +1,133 @@
-use regex::Regex;
-
 pub fn parse(input_data: String) -> String {
     /* Done in each part, as they differ */
     input_data
 }
 
 pub fn part_1(memory: &str) -> i64 {
-    let exp = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
-    exp.captures_iter(memory)
-        .map(|c| {
-            let n_1 = c[1].parse::<i64>().unwrap();
-            let n_2 = c[2].parse::<i64>().unwrap();
+    let len = memory.len();
+    let mem = memory.as_bytes();
 
-            n_1 * n_2
-        })
-        .sum()
-}
+    let mut i = 0;
+    let mut nums = vec![];
+    loop {
+        if i >= len - 1 {
+            break;
+        }
 
-pub fn part_2(memory: &str) -> i64 {
-    let exp = Regex::new(r"mul\((\d+),(\d+)\)|don\'t\(\)|do\(\)").unwrap();
+        if i + 3 < len && &mem[i..=i + 3] == b"mul(" {
+            i += 4;
 
-    let mut doing = true;
-    exp.captures_iter(memory)
-        .map(|c| match &c[0] {
-            "don't()" => {
-                doing = false;
-                0
-            }
-            "do()" => {
-                doing = true;
-                0
-            }
-            _ => {
-                if doing {
-                    let n_1 = c[1].parse::<i64>().unwrap();
-                    let n_2 = c[2].parse::<i64>().unwrap();
+            let consume_digits = |begin_index: usize, target_end: u8| -> (usize, Option<Vec<u8>>) {
+                if begin_index >= len {
+                    return (begin_index, None);
+                }
 
-                    n_1 * n_2
-                } else {
-                    0
+                let mut index = begin_index;
+
+                let num_str: Vec<u8> = mem[index..]
+                    .iter()
+                    .take_while(|&c| c.is_ascii_digit())
+                    .map(|c| {
+                        index += 1;
+                        *c
+                    })
+                    .collect();
+
+                (
+                    index,
+                    if index < len && mem[index] == target_end {
+                        Some(num_str)
+                    } else {
+                        None
+                    },
+                )
+            };
+
+            if let (new_index, Some(num_1_str)) = consume_digits(i, b',') {
+                i = new_index + 1;
+                if let (new_index, Some(num_2_str)) = consume_digits(i, b')') {
+                    i = new_index;
+
+                    if let Ok(num_1) = String::from_utf8(num_1_str).unwrap().parse::<i64>() {
+                        if let Ok(num_2) = String::from_utf8(num_2_str).unwrap().parse::<i64>() {
+                            nums.push(num_1 * num_2);
+                        }
+                    }
                 }
             }
-        })
-        .sum()
+        }
+
+        i += 1
+    }
+    nums.into_iter().sum()
+}
+
+// Same as part 1, but with "Don't()" and "Do()" detection.
+pub fn part_2(memory: &str) -> i64 {
+    let len = memory.len();
+    let mem = memory.as_bytes();
+
+    let mut i = 0;
+    let mut nums = vec![];
+    let mut doing = true;
+    loop {
+        if i >= len - 1 {
+            break;
+        }
+
+        if i + 3 < len && &mem[i..=i + 3] == b"mul(" {
+            i += 4;
+
+            let consume_digits = |begin_index: usize, target_end: u8| -> (usize, Option<Vec<u8>>) {
+                if begin_index >= len {
+                    return (begin_index, None);
+                }
+
+                let mut index = begin_index;
+
+                let num_str: Vec<u8> = mem[index..]
+                    .iter()
+                    .take_while(|&c| c.is_ascii_digit())
+                    .map(|c| {
+                        index += 1;
+                        *c
+                    })
+                    .collect();
+
+                (
+                    index,
+                    if index < len && mem[index] == target_end {
+                        Some(num_str)
+                    } else {
+                        None
+                    },
+                )
+            };
+
+            if let (new_index, Some(num_1_str)) = consume_digits(i, b',') {
+                i = new_index + 1;
+                if let (new_index, Some(num_2_str)) = consume_digits(i, b')') {
+                    i = new_index;
+
+                    if !doing {
+                        continue;
+                    }
+
+                    if let Ok(num_1) = String::from_utf8(num_1_str).unwrap().parse::<i64>() {
+                        if let Ok(num_2) = String::from_utf8(num_2_str).unwrap().parse::<i64>() {
+                            nums.push(num_1 * num_2);
+                        }
+                    }
+                }
+            }
+        } else if i + 6 < len && &mem[i..=i + 6] == b"don't()" {
+            doing = false;
+        } else if i + 1 < len && &mem[i..=i + 1] == b"do" {
+            doing = true;
+        }
+
+        i += 1
+    }
+
+    nums.into_iter().sum()
 }
