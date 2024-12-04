855f1cdc42136cfa06cbe015d8493f11d66791d5
keelus <hugomoreda@hotmail.com>
2024-12-04 12:10:05 +0100

    Added day 4 [part 2]

--- 2024/solutions/day04.rs
+++ 2024/solutions/day04.rs
@@ -5,6 +5,7 @@ pub enum Token {
 }
 
 static TARGET_WORD: &str = "XMAS";
+static TARGET_WORD_LEN: usize = TARGET_WORD.len();
 static REV_TARGET_WORD: &str = "SAMX";
 
 pub fn parse(input_data: String) -> Vec<String> {
@@ -13,6 +14,9 @@ pub fn parse(input_data: String) -> Vec<String> {
 
 pub fn part_1(lines: &[String]) -> i64 {
     let mut count = 0;
+
+    let line_count = lines.len();
+    let line_width = lines[0].len();
 
     // Check horizontals
     lines.iter().for_each(|l| {
@@ -32,10 +36,10 @@ pub fn part_1(lines: &[String]) -> i64 {
     });
 
     // Check verticals
-    for i in 0..=lines.len() - TARGET_WORD.len() {
-        for j in 0..lines[0].len() {
-            let mut word = Vec::with_capacity(TARGET_WORD.len());
-            for c_i in 0..TARGET_WORD.len() {
+    for i in 0..=line_count - TARGET_WORD_LEN {
+        for j in 0..line_width {
+            let mut word = Vec::with_capacity(TARGET_WORD_LEN);
+            for c_i in 0..TARGET_WORD_LEN {
                 word.push(lines[i + c_i].as_bytes()[j]);
             }
 
@@ -46,10 +50,10 @@ pub fn part_1(lines: &[String]) -> i64 {
     }
 
     // Check diagonals (left to right)
-    for i in 0..=lines.len() - TARGET_WORD.len() {
-        for j in 0..(lines[0].len() - TARGET_WORD.len() + 1) {
-            let mut word = Vec::with_capacity(TARGET_WORD.len());
-            for c_ij in 0..TARGET_WORD.len() {
+    for i in 0..=line_count - TARGET_WORD_LEN {
+        for j in 0..(line_width - TARGET_WORD_LEN + 1) {
+            let mut word = Vec::with_capacity(TARGET_WORD_LEN);
+            for c_ij in 0..TARGET_WORD_LEN {
                 word.push(lines[i + c_ij].as_bytes()[j + c_ij]);
             }
 
@@ -60,10 +64,10 @@ pub fn part_1(lines: &[String]) -> i64 {
     }
 
     // Check diagonals (right to left)
-    for i in 0..(lines.len() - TARGET_WORD.len() + 1) {
-        for j in (TARGET_WORD.len() - 1)..lines[0].len() {
-            let mut word = Vec::with_capacity(TARGET_WORD.len());
-            for c_ij in 0..TARGET_WORD.len() {
+    for i in 0..(line_count - TARGET_WORD_LEN + 1) {
+        for j in (TARGET_WORD_LEN - 1)..line_width {
+            let mut word = Vec::with_capacity(TARGET_WORD_LEN);
+            for c_ij in 0..TARGET_WORD_LEN {
                 word.push(lines[i + c_ij].as_bytes()[j - c_ij]);
             }
 
@@ -76,6 +80,34 @@ pub fn part_1(lines: &[String]) -> i64 {
     count as i64
 }
 
+const TARGET_WORD_2: &str = "MAS";
+static TARGET_WORD_2_LEN: usize = TARGET_WORD_2.len();
+const REV_TARGET_WORD_2: &str = "SAM";
+
 pub fn part_2(lines: &[String]) -> i64 {
-    0
+    let mut count = 0;
+
+    for i in 0..(lines.len() - TARGET_WORD_2_LEN + 1) {
+        for j in 0..(lines[0].len() - TARGET_WORD_2_LEN + 1) {
+            let mut word = Vec::with_capacity(TARGET_WORD_2_LEN);
+
+            for c_ij in 0..TARGET_WORD_2_LEN {
+                word.push(lines[i + c_ij].as_bytes()[j + c_ij]);
+            }
+
+            if word == TARGET_WORD_2.as_bytes() || word == REV_TARGET_WORD_2.as_bytes() {
+                word = Vec::with_capacity(TARGET_WORD_2_LEN);
+
+                for c_ij in 0..TARGET_WORD_2_LEN {
+                    word.push(lines[i + c_ij].as_bytes()[j + TARGET_WORD_2_LEN - 1 - c_ij]);
+
+                    if word == TARGET_WORD_2.as_bytes() || word == REV_TARGET_WORD_2.as_bytes() {
+                        count += 1
+                    }
+                }
+            }
+        }
+    }
+
+    count
 }
