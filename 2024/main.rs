a3cde63704628d96d7c6258b1a1bed1c7e768e16
keelus <hugomoreda@hotmail.com>
2024-12-06 13:43:35 +0100

    Added day 6 [parts 1 and 2].

--- 2024/main.rs
+++ 2024/main.rs
@@ -272,7 +272,7 @@ fn main() {
         3 => advent_day!(day03, args),
         4 => advent_day!(day04, args),
         5 => advent_day!(day05, args),
-        // 6 => advent_day!(day06, args),
+        6 => advent_day!(day06, args),
         // 7 => advent_day!(day07, args),
         // 8 => advent_day!(day08, args),
         // 9 => advent_day!(day09, args),
---
+++ 2024/solutions/day06.rs
@@ -0,0 +1,144 @@
+use std::collections::HashSet;
+
+#[derive(Eq, Hash, PartialEq, Copy, Clone)]
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Direction {
+    fn rotate_right_90(&self) -> Self {
+        match self {
+            Direction::Up => Direction::Right,
+            Direction::Right => Direction::Down,
+            Direction::Down => Direction::Left,
+            Direction::Left => Direction::Up,
+        }
+    }
+}
+
+type Position = (usize, usize);
+
+pub struct Input {
+    map: Vec<Vec<char>>,
+    start_pos: Position,
+}
+
+fn move_towards(pos: Position, dir: Direction) -> Position {
+    match dir {
+        // Allow overflows, as they will make the guard
+        // exit the map anyways.
+        Direction::Up => (pos.0.wrapping_sub(1), pos.1),
+        Direction::Down => (pos.0 + 1, pos.1),
+        Direction::Left => (pos.0, pos.1.wrapping_sub(1)),
+        Direction::Right => (pos.0, pos.1 + 1),
+    }
+}
+
+fn is_outside(pos: Position, map: &[Vec<char>]) -> bool {
+    let (height, width) = (map.len(), map[0].len());
+
+    pos.0 >= height || pos.1 >= width
+}
+
+fn collides(pos: Position, map: &[Vec<char>]) -> bool {
+    map.get(pos.0 as usize)
+        .and_then(|row| row.get(pos.1 as usize))
+        .is_some_and(|&c| c == '#')
+}
+
+pub fn parse(input_data: String) -> Input {
+    let start_pos = input_data
+        .lines()
+        .enumerate()
+        .fold(None, |start_pos, (i, l)| {
+            if start_pos.is_some() {
+                return start_pos;
+            }
+
+            l.chars()
+                .enumerate()
+                .find(|(_, c)| *c == '^')
+                .map(|(j, _)| (i, j))
+                .or(start_pos)
+        })
+        .expect("Starting position not found for the given input.");
+
+    let map = input_data.lines().map(|l| l.chars().collect()).collect();
+
+    Input { map, start_pos }
+}
+
+pub fn part_1(input: &Input) -> i64 {
+    let mut pos = input.start_pos;
+    let mut dir = Direction::Up;
+
+    let mut seen = HashSet::new();
+    seen.insert(pos);
+
+    loop {
+        let new_pos = move_towards(pos, dir);
+
+        if is_outside(new_pos, &input.map) {
+            break;
+        }
+
+        if collides(new_pos, &input.map) {
+            dir = dir.rotate_right_90();
+        } else {
+            pos = new_pos;
+            seen.insert(pos);
+        }
+    }
+
+    seen.len() as i64
+}
+
+pub fn part_2(input: &Input) -> i64 {
+    let mut loop_positions = HashSet::new();
+
+    // I am not sure on how to solve this one so,
+    // lets drop a bruteforce.
+    for i in 0..input.map.len() {
+        for j in 0..input.map[i].len() {
+            let new_obstacle_pos = (i, j);
+            if input.start_pos == new_obstacle_pos || input.map[i][j] == '#' {
+                continue;
+            }
+
+            let mut pos = input.start_pos;
+            let mut dir = Direction::Up;
+
+            let mut seen: HashSet<(Position, Direction)> = HashSet::new();
+            seen.insert((pos, dir));
+
+            // Now, it's just like part 1,
+            // but detecting loops.
+            loop {
+                let new_pos = move_towards(pos, dir);
+
+                if is_outside(new_pos, &input.map) {
+                    break;
+                }
+
+                if new_pos == new_obstacle_pos || collides(new_pos, &input.map) {
+                    dir = dir.rotate_right_90();
+                } else {
+                    pos = new_pos;
+
+                    if seen.contains(&(pos, dir)) {
+                        // Is a loop
+                        loop_positions.insert((i, j));
+                        break;
+                    } else {
+                        seen.insert((pos, dir));
+                    }
+                }
+            }
+        }
+    }
+
+    loop_positions.len() as i64
+}
