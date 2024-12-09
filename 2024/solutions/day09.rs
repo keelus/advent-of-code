4290b4ca6f4f8b5c124fadcc5cc5d66d3f570ffe
keelus <hugomoreda@hotmail.com>
2024-12-09 13:53:17 +0100

    Reduced/organized code & optimized parsing of day 9.

--- 2024/solutions/day09.rs
+++ 2024/solutions/day09.rs
@@ -1,24 +1,22 @@
 use std::collections::HashSet;
 
-type Part1BlockId = i64;
-type Part1Memory = Vec<Option<Part1BlockId>>;
-
-#[derive(Debug, Copy, Clone)]
-pub enum Part2Block {
-    Empty { size: usize },             // Block size
-    NonEmpty { size: usize, id: i64 }, // Block size, file id
-}
-pub type Part2Memory = Vec<Part2Block>;
-
 pub struct ParsedParts {
-    part_1: Part1Memory,
-    part_2: Part2Memory,
+    part_1: part_01::Memory,
+    part_2: part_02::Memory,
 }
 
 pub fn parse<'p>(input_data: String) -> ParsedParts {
-    let mut disk_line = String::new();
+    ParsedParts {
+        part_1: part_01::parse(&input_data),
+        part_2: part_02::parse(&input_data),
+    }
+}
 
-    let part_1_memory = {
+mod part_01 {
+    pub type BlockId = i64;
+    pub type Memory = Vec<Option<BlockId>>;
+
+    pub fn parse<'p>(input_data: &str) -> Memory {
         let mut memory = vec![];
 
         let mut is_blank = false;
@@ -29,12 +27,10 @@ pub fn parse<'p>(input_data: String) -> ParsedParts {
 
             if is_blank {
                 for _i in 0..num {
-                    disk_line.push_str(".");
                     memory.push(None);
                 }
             } else {
                 for _i in 0..num {
-                    disk_line.push_str(&disk_index.to_string());
                     memory.push(Some(disk_index));
                 }
                 disk_index += 1;
@@ -44,9 +40,84 @@ pub fn parse<'p>(input_data: String) -> ParsedParts {
         }
 
         memory
-    };
+    }
 
-    let part_2_memory = {
+    pub fn leftmost_free_block_index(memory: &Memory) -> Option<usize> {
+        let mut first_empty = None;
+
+        for (i, block) in memory.iter().enumerate() {
+            if block.is_none() {
+                if first_empty.is_none() {
+                    first_empty = Some(i);
+                }
+            } else if first_empty.is_some() {
+                return first_empty;
+            }
+        }
+
+        return None;
+    }
+
+    pub fn rightmost_block(memory: &Memory) -> (usize, BlockId) {
+        for (i, block) in memory.iter().enumerate().rev() {
+            if !block.is_none() {
+                return (i, block.unwrap());
+            }
+        }
+
+        unreachable!("Found empty only");
+    }
+
+    pub fn calculate_checksum(memory: &Memory) -> i64 {
+        memory
+            .iter()
+            .enumerate()
+            .take_while(|&(_, b)| b.is_some())
+            .map(|(i, b)| (i as i64) * b.unwrap())
+            .sum()
+    }
+}
+
+// Will return the leftmost free block index, only if the
+// disk is non contiguous.
+pub fn part_1(parsed_parts: &ParsedParts) -> i64 {
+    let mut memory = parsed_parts.part_1.clone();
+
+    while let Some(leftmost_free_index) = part_01::leftmost_free_block_index(&memory) {
+        // Optimization TODO:
+        // Save the leftmost and rightmos indexes? So is easier where begin
+
+        let (rightmost_index, block_id) = part_01::rightmost_block(&memory);
+
+        memory[leftmost_free_index] = Some(block_id);
+        memory[rightmost_index] = None;
+    }
+
+    part_01::calculate_checksum(&memory)
+}
+
+mod part_02 {
+    #[derive(Debug, Copy, Clone)]
+    pub struct Block {
+        pub size: usize,
+        pub id: Option<i64>,
+    }
+
+    impl Block {
+        pub fn new(size: usize, id: Option<i64>) -> Self {
+            Self { size, id }
+        }
+        pub fn new_empty(size: usize) -> Self {
+            Self { size, id: None }
+        }
+        pub fn is_empty(&self) -> bool {
+            self.id.is_none()
+        }
+    }
+
+    pub type Memory = Vec<Block>;
+
+    pub fn parse<'p>(input_data: &str) -> Memory {
         let mut memory = vec![];
 
         let mut is_blank = false;
@@ -56,18 +127,9 @@ pub fn parse<'p>(input_data: String) -> ParsedParts {
             let num: usize = element.to_string().parse().unwrap();
 
             if is_blank {
-                for _i in 0..num {
-                    disk_line.push_str(".");
-                }
-                memory.push(Part2Block::Empty { size: num });
+                memory.push(Block::new_empty(num));
             } else {
-                for _i in 0..num {
-                    disk_line.push_str(&disk_index.to_string());
-                }
-                memory.push(Part2Block::NonEmpty {
-                    size: num,
-                    id: disk_index,
-                });
+                memory.push(Block::new(num, Some(disk_index)));
                 disk_index += 1;
             }
 
@@ -75,184 +137,103 @@ pub fn parse<'p>(input_data: String) -> ParsedParts {
         }
 
         memory
-    };
-
-    // println!("{:?}", part_2_memory);
-
-    ParsedParts {
-        part_1: part_1_memory,
-        part_2: part_2_memory,
     }
-}
 
-// Will return the leftmost free block index, only if the
-// disk is non contiguous.
-fn leftmost_free_block_index(memory: &Part1Memory) -> Option<usize> {
-    let mut first_empty = None;
-
-    for (i, block) in memory.iter().enumerate() {
-        if block.is_none() {
-            if first_empty.is_none() {
-                first_empty = Some(i);
+    pub fn is_space_available(memory: &Memory, size_needed: usize) -> Option<usize> {
+        for i in 0..memory.len() {
+            if memory[i].is_empty() && memory[i].size >= size_needed {
+                return Some(i);
             }
-        } else if first_empty.is_some() {
-            return first_empty;
         }
+
+        None
     }
 
-    return None;
-}
+    pub fn find_first_non_empty_merged(memory: &Memory) -> Option<usize> {
+        for i in 0..memory.len() - 1 {
+            let cur = memory[i];
+            let next = memory[i + 1];
 
-fn rightmost_block(memory: &Part1Memory) -> (usize, Part1BlockId) {
-    for (i, block) in memory.iter().enumerate().rev() {
-        if !block.is_none() {
-            return (i, block.unwrap());
+            if cur.is_empty() && next.is_empty() {
+                return Some(i);
+            }
         }
+
+        None
     }
 
-    unreachable!("Found empty only");
-}
+    pub fn merge_empty_blocks(mut memory: Memory) -> Memory {
+        if let Some(i) = find_first_non_empty_merged(&memory) {
+            // Merge with right one
+            let next_block = memory.remove(i + 1);
 
-fn calculate_checksum(memory: &Part1Memory) -> i64 {
-    memory
-        .iter()
-        .enumerate()
-        .take_while(|&(_, b)| b.is_some())
-        .map(|(i, b)| (i as i64) * b.unwrap())
-        .sum()
-}
-
-pub fn part_1(parsed_parts: &ParsedParts) -> i64 {
-    let mut memory = parsed_parts.part_1.clone();
-
-    while let Some(leftmost_free_index) = leftmost_free_block_index(&memory) {
-        // Optimization TODO:
-        // Save the leftmost and rightmos indexes? So is easier where begin
-
-        let (rightmost_index, block_id) = rightmost_block(&memory);
-
-        memory[leftmost_free_index] = Some(block_id);
-        memory[rightmost_index] = None;
-    }
-
-    calculate_checksum(&memory)
-}
-
-pub fn is_space_available(memory: &Part2Memory, size_needed: usize) -> Option<usize> {
-    for i in 0..memory.len() {
-        match memory[i] {
-            Part2Block::NonEmpty { .. } => continue,
-            Part2Block::Empty { size } => {
-                if size >= size_needed {
-                    return Some(i);
+            if memory[i].is_empty() {
+                if next_block.is_empty() {
+                    memory[i].size += next_block.size;
                 }
             }
+
+            merge_empty_blocks(memory)
+        } else {
+            memory
         }
     }
 
-    None
-}
-
-pub fn print_memory(memory: &Part2Memory) {
-    let mut output = String::new();
-
-    for block in memory.iter() {
-        match block {
-            Part2Block::Empty { size } => {
-                for _i in 0..*size {
-                    output.push_str(".");
+    pub fn calculate_checksum_2(memory: &Memory) -> i64 {
+        let mut global_index = 0i64;
+        memory
+            .iter()
+            .map(|b| {
+                if let Some(id) = b.id {
+                    let mut loc_sum = 0;
+                    for _i in 0..b.size {
+                        loc_sum += global_index * id;
+                        global_index += 1 as i64;
+                    }
+                    loc_sum
+                } else {
+                    global_index += b.size as i64;
+                    0
                 }
-            }
-            Part2Block::NonEmpty { id, size } => {
-                for _i in 0..*size {
-                    if *id < 10 {
+            })
+            .sum()
+    }
+
+    pub fn print_memory(memory: &Memory) {
+        let mut output = String::new();
+
+        for block in memory.iter() {
+            if let Some(id) = block.id {
+                for _i in 0..block.size {
+                    if id < 10 {
                         output.push_str(&id.to_string());
                     } else {
                         output.push_str(&format!("({id})"));
                     }
                 }
-            }
-        }
-    }
-
-    println!("{output}");
-}
-
-pub fn find_first_non_empty_merged(memory: &Part2Memory) -> Option<usize> {
-    for i in 0..memory.len() - 1 {
-        let cur = memory[i];
-        let next = memory[i + 1];
-
-        if let Part2Block::Empty { .. } = cur {
-            if let Part2Block::Empty { .. } = next {
-                return Some(i);
-            }
-        }
-    }
-
-    None
-}
-
-pub fn merge_empty_blocks(mut memory: Part2Memory) -> Part2Memory {
-    if let Some(i) = find_first_non_empty_merged(&memory) {
-        // Merge with right one
-        let next_block = memory.remove(i + 1);
-
-        if let Part2Block::Empty {
-            size: ref mut cur_size,
-        } = memory[i]
-        {
-            if let Part2Block::Empty { size: next_size } = next_block {
-                *cur_size += next_size;
-            }
-        }
-
-        merge_empty_blocks(memory)
-    } else {
-        memory
-    }
-}
-
-fn calculate_checksum_2(memory: &Part2Memory) -> i64 {
-    let mut global_index = 0i64;
-    memory
-        .iter()
-        .map(|b| match b {
-            Part2Block::NonEmpty { size, id } => {
-                let mut loc_sum = 0;
-                for _i in 0..*size {
-                    loc_sum += global_index * id;
-                    global_index += 1 as i64;
+            } else {
+                for _i in 0..block.size {
+                    output.push_str(".");
                 }
-                loc_sum
             }
-            Part2Block::Empty { size } => {
-                global_index += *size as i64;
-                0
-            }
-        })
-        .sum()
+        }
+
+        println!("{output}");
+    }
 }
 
 pub fn part_2(parsed_parts: &ParsedParts) -> i64 {
     let mut memory = parsed_parts.part_2.clone();
 
-    let non_empty_block_len = memory
-        .iter()
-        .filter(|block| {
-            if let Part2Block::Empty { .. } = block {
-                return false;
-            } else {
-                return true;
-            }
-        })
-        .count();
+    let non_empty_block_len = memory.iter().filter(|block| !block.is_empty()).count();
 
     let mut viewed_blocks = HashSet::new();
 
     loop {
         for (i, &file_block) in memory.iter().enumerate().rev() {
-            if let Part2Block::NonEmpty { size, id } = file_block {
+            let (id, size) = { (file_block.id, file_block.size) };
+
+            if let Some(id) = id {
                 if viewed_blocks.contains(&id) {
                     continue;
                 }
@@ -261,36 +242,31 @@ pub fn part_2(parsed_parts: &ParsedParts) -> i64 {
 
                 let mut added_block = false;
                 let mut new_memory = memory.clone();
-                if let Some(empty_index) = is_space_available(&memory, size) {
+                if let Some(empty_index) = part_02::is_space_available(&memory, size) {
                     if empty_index < i {
                         // Do the replacement
                         let empty_block = memory[empty_index];
-                        new_memory[i] = Part2Block::Empty { size };
+                        new_memory[i] = part_02::Block::new_empty(size);
                         added_block = true;
 
-                        if let Part2Block::Empty {
-                            size: empty_block_size,
-                        } = empty_block
-                        {
-                            // Replace / insert the block into the empty space
-                            if empty_block_size == size {
-                                new_memory[empty_index] = Part2Block::NonEmpty { size, id };
-                            } else {
-                                let new_empty_block = Part2Block::Empty {
-                                    size: empty_block_size - size,
-                                };
-                                new_memory[empty_index] = Part2Block::NonEmpty { size, id };
-                                new_memory.insert(empty_index + 1, new_empty_block);
-                            }
+                        let empty_block_size = empty_block.size;
+
+                        // Replace / insert the block into the empty space
+                        if empty_block_size == size {
+                            new_memory[empty_index] = part_02::Block::new(size, Some(id));
                         } else {
-                            panic!("Returned a non empty instead of an empty?");
+                            let new_empty_block =
+                                part_02::Block::new_empty(empty_block_size - size);
+
+                            new_memory[empty_index] = part_02::Block::new(size, Some(id));
+                            new_memory.insert(empty_index + 1, new_empty_block);
                         }
                     }
                 }
 
                 if added_block {
                     // Merge contiguous empty blocks
-                    let new_memory = merge_empty_blocks(new_memory);
+                    let new_memory = part_02::merge_empty_blocks(new_memory);
                     memory = new_memory;
                     break;
                 }
@@ -302,6 +278,5 @@ pub fn part_2(parsed_parts: &ParsedParts) -> i64 {
         }
     }
 
-    // print_memory(&memory);
-    calculate_checksum_2(&memory)
+    part_02::calculate_checksum_2(&memory)
 }
